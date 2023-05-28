0a1976a0116d9133087ad34ce23a7febcbd6f43e
Edoardo Mich <71270665+edoardomich@users.noreply.github.com>
2023-05-28 03:51:00 +0200

    Clean confs

--- Configs/snort_ips.lua
+++ Configs/snort_ips.lua
@@ -2,58 +2,12 @@
 -- Snort++ configuration
 ---------------------------------------------------------------------------
 
--- there are over 200 modules available to tune your policy.
--- many can be used with defaults w/o any explicit configuration.
--- use this conf as a template for your specific configuration.
-
--- 1. configure defaults
--- 2. configure inspection
--- 3. configure bindings
--- 4. configure performance
--- 5. configure detection
--- 6. configure filters
--- 7. configure outputs
--- 8. configure tweaks
-
----------------------------------------------------------------------------
--- 1. configure defaults
----------------------------------------------------------------------------
-
--- HOME_NET and EXTERNAL_NET must be set now
--- setup the network addresses you are protecting
-HOME_NET = 'any'
-
--- set up the external network addresses.
--- (leave as "any" in most situations)
-EXTERNAL_NET = 'any'
-
---include 'snort_defaults.lua'
-
----------------------------------------------------------------------------
--- 2. configure inspection
----------------------------------------------------------------------------
-
--- mod = { } uses internal defaults
--- you can see them with snort --help-module mod
-
--- mod = default_mod uses external defaults
--- you can see them in snort_defaults.lua
-
----------------------------------------------------------------------------
--- 5. configure detection
----------------------------------------------------------------------------
-
-references = default_references
-classifications = default_classifications
 daq =
 {
    modules =
    {
        {
            name = 'afpacket',
-           
-           --For netfilter (NFQUEUE) based IPS
-           --name = 'nfq',
            
            mode = 'inline',
 	   variables =
@@ -65,11 +19,6 @@ daq =
    module_dirs =
    {
        '/usr/local/lib/daq/',
-   },
-   inputs =
-   {
-	'eth0:eth1',
-	'eth0:eth2'
    }
 }
 
@@ -80,9 +29,9 @@ ips =
     --action_override = 'reject',
 
 --    rules =
-    --[[ 
+--    [[ 
 --	include /root/snort/rules/exercise20.rules
-    --]]
+--    ]]
     -- use this to enable decoder and inspector alerts
     --enable_builtin_rules = true,
 
@@ -91,88 +40,3 @@ ips =
     -- (see also related path vars at the top of snort_defaults.lua)
 
 }
---reject = { control="all", reset="both" }
-
---normalizer = { tcp = { ips = true, } }
--- use these to configure additional rule actions
--- react = { }
--- reject = { }
-
--- use this to enable payload injection utility
--- payload_injector = { }
-
----------------------------------------------------------------------------
--- 6. configure filters
----------------------------------------------------------------------------
-
--- below are examples of filters
--- each table is a list of records
-
---[[
-suppress =
-{
-    -- don't want to any of see these
-    { gid = 1, sid = 1 },
-
-    -- don't want to see anything for a given host
-    { track = 'by_dst', ip = '1.2.3.4' }
-
-    -- don't want to see these for a given host
-    { gid = 1, sid = 2, track = 'by_dst', ip = '1.2.3.4' },
-}
---]]
-
---[[
-event_filter =
-{
-    -- reduce the number of events logged for some rules
-    { gid = 1, sid = 1, type = 'limit', track = 'by_src', count = 2, seconds = 10 },
-    { gid = 1, sid = 2, type = 'both',  track = 'by_dst', count = 5, seconds = 60 },
-}
---]]
-
---[[
-rate_filter =
-{
-    -- alert on connection attempts from clients in SOME_NET
-    { gid = 135, sid = 1, track = 'by_src', count = 5, seconds = 1,
-      new_action = 'alert', timeout = 4, apply_to = '[$SOME_NET]' },
-
-    -- alert on connections to servers over threshold
-    { gid = 135, sid = 2, track = 'by_dst', count = 29, seconds = 3,
-      new_action = 'alert', timeout = 1 },
-}
---]]
-
----------------------------------------------------------------------------
--- 7. configure outputs
----------------------------------------------------------------------------
-
--- event logging
--- you can enable with defaults from the command line with -A <alert_type>
--- uncomment below to set non-default configs
---alert_csv = { }
---alert_fast = { }
---alert_full = { }
---alert_sfsocket = { }
---alert_syslog = { }
---unified2 = { }
-
--- packet logging
--- you can enable with defaults from the command line with -L <log_type>
---log_codecs = { }
---log_hext = { }
---log_pcap = { }
-
--- additional logs
---packet_capture = { }
---file_log = { }
-
----------------------------------------------------------------------------
--- 8. configure tweaks
----------------------------------------------------------------------------
-
-if ( tweaks ~= nil ) then
-    include(tweaks .. '.lua')
-end
-
